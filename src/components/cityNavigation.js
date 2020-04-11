9519b621f41466d97bdad1cd52c14348c81efdc7
Ewa Trzemżalska <trzemson@gmail.com>
2020-04-11 18:35:39 +0200

    Remove reduntant console.log

--- src/components/cityNavigation.js
+++ src/components/cityNavigation.js
@@ -3,7 +3,6 @@ import {Link} from "gatsby"
 import stringUtils from "../utils/string.js"
 
 export default ({ cities }) => {
-  console.log(cities)
   const Cities = cities.map(city => {
     return <Link key={city} to={"/" + stringUtils.escapeDiacritics(city.toLowerCase())}>{city}</Link>
   })
