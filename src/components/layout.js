83e07fa1453b2340f9a407b63c614c6a3b267493
Ewa Trzemżalska <trzemson@gmail.com>
2020-04-19 21:32:09 +0200

    Add Modal to open How to order button

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,8 @@
 import React from "react"
 import { Link, useStaticQuery, graphql } from "gatsby"
 import 'prismjs/themes/prism-okaidia.css';
+import { ModalProvider } from 'styled-react-modal'
+
 
 export default ({ children }) => {
   const data = useStaticQuery(
@@ -15,13 +17,15 @@ export default ({ children }) => {
     `
   )
   return (
-    <div className="site-wrapper">
+    <div className="site-wrapper" id="main">
       <header className="site-header">
         <div className="site-title">
           <Link to="/">{data.site.siteMetadata.title}</Link>
         </div>
       </header>
-      {children}
+      <ModalProvider>
+        {children}
+      </ModalProvider>
       <footer className="site-footer">
         <p>Crafted with <span role="img" aria-label="love">❤️</span> by <a href="https://github.com/EwaTrzemzalska" target="_blank">Ewa Trzemżalska</a> (source on <a href="https://github.com/EwaTrzemzalska/japodjade" target="_blank">GitHub)</a></p>
         <p>Template by <a href="https://w3layouts.com" target="_blank">W3Layouts</a></p>
--- src/components/post-link.js
+++ src/components/post-link.js
@@ -1,21 +1,59 @@
 import React from "react"
+import Modal from 'styled-react-modal'
 
-const PostLink = ({ post }) => (
-  
-  <article className="card ">
-    <div className="img-container">
-      <img src={post.data.Logo.localFiles[0].publicURL} alt="Logo"></img>
+const PostLink = ({ post }) => {
+
+  const [isOpen, setIsOpen] = React.useState(false)
+
+  function toggleModal(e) {
+    setIsOpen(!isOpen)
+  }
+
+  const StyledModal = Modal.styled`
+    width: 40rem;
+    border-radius: 9px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    background-color: white;
+    position: relative;
+  `
+
+  return (
+    <div>
+      <article className="card">
+        <div className="img-container">
+          <img src={post.data.Logo.localFiles[0].publicURL} alt="Logo"></img>
+        </div>
+        <header>
+          <h2 className="post-title">{post.data.Nazwa}</h2>
+          <div className="post-meta">
+            {post.data.MiastoTrimmed}
+          </div>
+        </header>
+        <p>{post.data.Opis}</p>
+        <div>
+          <button onClick={toggleModal}>Jak zamawiać?</button>
+          <StyledModal
+            isOpen={isOpen}
+            onBackgroundClick={toggleModal}
+            onEscapeKeydown={toggleModal} className="card">
+            <button onClick={toggleModal} aria-label="Close" className="close">&times;</button>
+            <div className="img-container">
+              <img src={post.data.Logo.localFiles[0].publicURL} alt="Logo"></img>
+            </div>
+            <h2 className="mb-5">Jak zamawiać z {post.data.Nazwa}?</h2>
+            <p className="mb-30">{post.data.Kontakt}</p>
+            <p className="grey-bg mb-30">Nie zapomnij wspomnieć, że znalazłeś {post.data.Nazwa} na <b>japodjade.pl</b> podczas składania zamówienia!</p>
+            <p className="mb-0"><b>Pozostałe informacje:</b></p>
+            <p><b>Strona www: </b><a href={post.data.Link} target="_blank">{post.data.Link}</a></p>
+          </StyledModal>
+        </div>
+        <div>
+          <a href={post.data.Link} target="_blank">{post.data.Link}</a>
+        </div>
+      </article>
     </div>
-    <header>
-      <h2 className="post-title">{post.data.Nazwa}</h2>
-      <div className="post-meta">
-        {post.data.MiastoTrimmed}
-      </div>
-    </header>
-    <p>{post.data.Opis}</p>
-    <h4 className="subtitle">Zamówienia</h4>
-    <p>{post.data.Kontakt}</p>
-    <a href={post.data.Link} target="_blank">{post.data.Link}</a>
-  </article>
-)
+  )
+}
 export default PostLink
